207cccda446106ad60d768d12ee160e901ab5df5
Tikhon <loki_deleoni@live.com>
2023-04-08 02:39:43 +0300

    create calculator for int numbers

---
+++ Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿int enterNumber(string text) // функция для ввода числового значения пользователем
+{
+    Console.Write($"{text}: ");
+    int result;
+    while (true)
+    {
+        string Number = Console.ReadLine();
+        if (int.TryParse(Number, out result))
+            return result;
+
+        System.Console.WriteLine($"Error. {text}");
+    }
+    return 0;
+}
+
+
+void Calculator (int a, int b)
+{
+    bool done = false;
+    while(!done)
+    {
+        Console.WriteLine("Please chouse which operation would you like to do?");
+        System.Console.WriteLine("+, -, *, /");
+        string Operator = Console.ReadLine();
+
+            switch (Operator)
+            {
+                case "+":
+                    System.Console.WriteLine($"{a} {Operator} {b} = {a + b}");
+                    done = true;
+                    break;
+
+                case "-":
+                    System.Console.WriteLine($"{a} {Operator} {b} = {a - b}");
+                    done = true;
+                    break;
+
+                case "*":
+                    System.Console.WriteLine($"{a} {Operator} {b} = {a * b}");
+                    done = true;
+                    break;
+
+                case "/":
+                    System.Console.WriteLine($"{a} {Operator} {b} = {a / b}");
+                    done = true;
+                    break;
+                
+                default:
+                    System.Console.WriteLine("Input invalid operator");
+                    break;
+        }
+    }
+}
+
+
+
+int a = enterNumber("Please enter first number: ");
+
+int b = enterNumber("Please enter second number: ");
+
+Calculator(a, b);
